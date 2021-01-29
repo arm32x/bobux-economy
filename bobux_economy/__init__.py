e06c5c5fc26ad37db5706ddfbbb42f2d542138f0
arm32x <arm32.x@gmail.com>
2021-01-29 16:23:54 -0500

    Add command to change prefix per guild.

--- bobux_economy/__init__.py
+++ bobux_economy/__init__.py
@@ -1,30 +1,38 @@
 import sqlite3
+from typing import *
 
 import discord
+from discord.ext import commands
 
 import database
 
 cursor = database.connection.cursor()
 database.initialize(cursor)
 
-client = discord.Client()
+def determine_prefix(_, message: discord.Message):
+    cursor.execute("SELECT prefix FROM guilds WHERE id = ?;", (message.guild.id, ))
+    guild_row: Optional[sqlite3.Row] = cursor.fetchone()
+    if guild_row is None:
+        return "b$"
+    else:
+        return guild_row["prefix"]
 
-@client.event
+bot = commands.Bot(command_prefix=determine_prefix)
+
+@bot.event
 async def on_ready():
     print("Ready.")
 
-@client.event
-async def on_message(message: discord.Message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def ping(ctx: commands.Context):
+    await ctx.channel.send("Pong!")
 
-    cursor.execute("INSERT OR IGNORE INTO guilds(id) VALUES(?);", (message.guild.id, ))
-    database.connection.commit()
-    cursor.execute("SELECT * FROM guilds WHERE id = ?;", (message.guild.id, ))
-    guild_row: sqlite3.Row = cursor.fetchone()
-
-    prefix = guild_row["prefix"]
+@bot.command()
+async def prefix(ctx: commands.Context, new_prefix: str):
+    cursor.execute("INSERT INTO guilds(id, prefix) VALUES(?, ?) ON CONFLICT(id) DO UPDATE SET prefix=excluded.prefix;", (ctx.guild.id, new_prefix))
+    cursor.commit()
+    await ctx.channel.send(f"Updated prefix to `{new_prefix}`.")
 
 with open("../token.txt", "r") as token_file:
     token = token_file.read()
-    client.run(token)
+    bot.run(token)
