98850b676092daf35fad6fc6560ebbc3576e4165
arm32x <arm32.x@gmail.com>
2022-10-31 15:46:32 -0400

    Use sqlite3.Row objects instead of tuples

--- bobux_economy/database.py
+++ bobux_economy/database.py
@@ -5,6 +5,11 @@ import yoyo
 
 connection = sqlite3.connect("data/bobux.db", detect_types=sqlite3.PARSE_DECLTYPES)
 
+# This will not affect existing code since sqlite3.Row objects support
+# the same operations as tuples. However, this allows new code to take
+# advantage of features provided by sqlite3.Row.
+connection.row_factory = sqlite3.Row
+
 def migrate():
     yoyo_backend = yoyo.get_backend("sqlite:///data/bobux.db")
     yoyo_migrations = yoyo.read_migrations("migrations")
