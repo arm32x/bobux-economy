128b649f32fe2ea38df1c47c89d95774a252b645
arm32x <arm32x@arm32.ax>
2023-03-22 14:59:52 -0400

    Reimplement balance module with transactions API
    This commit changes the implementation of the functions in the balance
    module to defer to the new transactions API introduced a couple of
    commits ago. It also deprecates these functions, since the new
    transactions API is meant to replace them.

--- bobux_economy/balance.py
+++ bobux_economy/balance.py
@@ -1,90 +1,100 @@
+import dataclasses
 import math
 from typing import Tuple
+from typing_extensions import deprecated
 
 import aiosqlite
 import disnake
-
-from bobux_economy.utils import UserFacingError
-
-
-class InsufficientFundsError(UserFacingError):
-    def __init__(self):
-        super().__init__("Insufficient funds")
-
-class NegativeAmountError(UserFacingError):
-    def __init__(self):
-        super().__init__("Amount must not be negative")
+from bobux_economy import utils
+from bobux_economy.bobux import Account, Bobux
+from bobux_economy.transactions import create_transaction
 
 
-async def get(db_connection: aiosqlite.Connection, member: disnake.Member) -> Tuple[int, bool]:
-    async with db_connection.cursor() as db_cursor:
-        await db_cursor.execute("""
-            SELECT balance, spare_change FROM members WHERE id = ? AND guild_id = ?;
-        """, (member.id, member.guild.id))
-        row = await db_cursor.fetchone()
+@deprecated("Use Account.from_member(...).get_balance(...) instead.")
+async def get(
+    db_connection: aiosqlite.Connection, member: disnake.Member
+) -> Tuple[int, bool]:
+    balance = await Account.from_member(member).get_balance(db_connection)
+    return dataclasses.astuple(balance)
 
-        if row is not None:
-            return (row["balance"], row["spare_change"])
+
+@deprecated("Use transactions.create_transaction(...) instead.")
+async def set(
+    db_connection: aiosqlite.Connection,
+    member: disnake.Member,
+    amount: int,
+    spare_change: bool,
+):
+    async with utils.db_transaction(db_connection):
+        account = Account.from_member(member)
+
+        old_balance = await account.get_balance(db_connection)
+        new_balance = Bobux(amount, spare_change)
+        transaction_amount = new_balance - old_balance
+
+        if transaction_amount < Bobux.ZERO:
+            transaction_amount = -transaction_amount
+            source, destination = account, None
         else:
-            return (0, False)
+            source, destination = None, account
 
-async def set(db_connection: aiosqlite.Connection, member: disnake.Member, amount: int, spare_change: bool):
-    async with db_connection.cursor() as db_cursor:
-        await db_cursor.execute("""
-            INSERT INTO members(id, guild_id, balance, spare_change) VALUES(?, ?, ?, ?)
-                ON CONFLICT(id, guild_id) DO UPDATE SET balance = excluded.balance, spare_change = excluded.spare_change;
-        """, (member.id, member.guild.id, amount, spare_change))
-        await db_connection.commit()
-
-async def add(db_connection: aiosqlite.Connection, member: disnake.Member, amount: int, spare_change: bool):
-    if amount < 0:
-        raise NegativeAmountError()
-
-    balance, balance_spare_change = await get(db_connection, member)
-
-    balance += amount
-    if spare_change and balance_spare_change:
-        balance += 1
-    balance_spare_change ^= spare_change
-
-    await set(db_connection, member, balance, balance_spare_change)
-
-async def subtract(db_connection: aiosqlite.Connection, member: disnake.Member, amount: int, spare_change: bool, allow_overdraft=False):
-    if amount < 0:
-        raise NegativeAmountError()
-
-    balance, balance_spare_change = await get(db_connection, member)
-
-    if not allow_overdraft and balance < amount or balance == amount and spare_change and not balance_spare_change:
-        raise InsufficientFundsError()
-
-    balance -= amount
-    if spare_change and not balance_spare_change:
-        balance -= 1
-    balance_spare_change ^= spare_change
-
-    await set(db_connection, member, balance, balance_spare_change)
+        await create_transaction(db_connection, source, destination, transaction_amount)
 
 
+@deprecated("Use transactions.create_transaction(...) instead.")
+async def add(
+    db_connection: aiosqlite.Connection,
+    member: disnake.Member,
+    amount: int,
+    spare_change: bool,
+):
+    account = Account.from_member(member)
+    transaction_amount = Bobux(amount, spare_change)
+    await create_transaction(db_connection, None, account, transaction_amount)
+
+
+@deprecated("Use transactions.create_transaction(...) instead.")
+async def subtract(
+    db_connection: aiosqlite.Connection,
+    member: disnake.Member,
+    amount: int,
+    spare_change: bool,
+    allow_overdraft=False,
+):
+    account = Account.from_member(member)
+    transaction_amount = Bobux(amount, spare_change)
+    await create_transaction(db_connection, account, None, transaction_amount, allow_overdraft)
+
+
+@deprecated("Use Bobux.from_float(...) instead.")
 def from_float(amount: float) -> Tuple[int, bool]:
     return int(amount), not amount.is_integer()
 
+
+@deprecated("No replacement.")
 def from_float_floor(amount: float) -> Tuple[int, bool]:
     rounded = math.floor(amount * 2) / 2
     return from_float(rounded)
 
+
+@deprecated("No replacement.")
 def from_float_ceil(amount: float) -> Tuple[int, bool]:
     rounded = math.ceil(amount * 2) / 2
     return from_float(rounded)
 
+
+@deprecated("No replacement.")
 def from_float_round(amount: float) -> Tuple[int, bool]:
     rounded = round(amount * 2) / 2
     return from_float(rounded)
 
+
+@deprecated("Use Bobux(...).to_float() instead.")
 def to_float(amount: int, spare_change: bool) -> float:
     return amount + (0.5 if spare_change else 0)
 
 
+@deprecated("Use str(Bobux(...)) instead.")
 def to_string(amount: int, spare_change: bool) -> str:
     if spare_change:
         return f"{amount} bobux and some spare change"
